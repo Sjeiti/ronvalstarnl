99b51ed4e688fcf79049a20125e9b49782fa41c4
Ron Valstar <ron@ronvalstar.nl>
2019-08-23 16:37:00 +0200

    testing cloudinary media cdn

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,5 @@
 export const VERSION = _VERSION
 export const ENV = _ENV
 export const ENV_DEVELOPMENT = ENV['development']||false
+export const MEDIA_URI = 'https://res.cloudinary.com/dn1rmdjs5/image/upload/v1566568756/rv/'
 console.log('VERSION,ENV',VERSION,ENV) // todo: remove log
