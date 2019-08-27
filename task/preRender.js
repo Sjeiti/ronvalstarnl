d250e7544e3fbbb0e18b6c290a9dcaf1e82bd96d
Ron Valstar <ron@ronvalstar.nl>
2019-08-28 00:07:49 +0200

    preRender saves

--- task/preRender.js
+++ task/preRender.js
@@ -1,22 +1,45 @@
 const puppeteer = require('puppeteer')
+const {URL} = require('url')
 
 const utils = require('./util/utils.js')
 const {save} = utils
 
-async function run() {
+const targetPath = 'temp/renders/'
+const baseUri = 'http://localhost:7047'
+
+const siteMap = new Map()
+const todos = []
+
+async function run(currentUri) {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
 
-  await page.goto('http://localhost:7047')
-  // await page.screenshot({ path: 'temp/screenshot.png' })
-
-  const text = await page.evaluate(() => [...document.querySelectorAll('a')].map(m=>m.getAttribute('href')).filter(uri=>!/^https?:\/\//i.test(uri)))
-  console.log('anchors',text.join(' | ')) // todo: remove log
-
+  await page.goto(currentUri)
   const renderedContent = await page.evaluate(() => new XMLSerializer().serializeToString(document))
-  save('temp/renders/index.html', renderedContent)
-
+  const uris = await page.evaluate(() => [...document.querySelectorAll('a')].map(m=>m.getAttribute('href')).filter(uri=>!/^https?:\/\//i.test(uri)))
+  // await page.screenshot({ path: 'temp/screenshot.png' })
   browser.close()
+
+  // todo: no 404's
+  // todo: no searches?
+
+  save(`${targetPath}${currentUri.replace(baseUri,'')}/index.html`, renderedContent)
+  siteMap.set(currentUri, renderedContent)
+
+  uris.forEach(uri=>{
+    // const isValid = !/^mailto:|^tel:/.test(uri)
+    const isValid = /^\//.test(uri)
+    const uriConcat = (baseUri+uri)
+    const isMapped = !!siteMap.get(uriConcat)
+    if(isValid&&!isMapped){
+      siteMap.set(currentUri, true) // awaiting... to prevent concurrent fetches
+      todos.push(uriConcat)
+    }
+    // console.log('uri',isMapped,uri) // todo: remove log
+  })
+
+  const todoUri = todos.pop()
+  todoUri&&run(todoUri)
 }
 
-run()
+run(baseUri)
