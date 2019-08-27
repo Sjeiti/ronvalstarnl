facfc69697b0eb27f6675a46934ea78e752ec735
Ron Valstar <ron@ronvalstar.nl>
2019-08-27 07:36:19 +0200

    home styling

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -11,22 +11,30 @@ add(
         //
         // projects // todo duplicate code in `view/projects.js`
         const projectHighlight = ['strange-attractors-javascript', 'kees-kroot', 'disconnect', 'project-invoice'].map(slug=>projects.filter(p=>p.slug===slug).pop())
-        view.expandAppend(`section>(h2.section-title>small{projects}+{built})+ul.unstyled.projects>(${projectHighlight.map(
+        view.expandAppend(`section.built>(h2.section-title>small{projects}+{built})+ul.unstyled.projects>(${projectHighlight.map(
               project=>`(li${project.categories.map(c=>`.cat-${c}`).join('')}[style="background-image:url(${MEDIA_URI_THUMB+project.thumbnail})"]>a[href="/project/${project.slug}"]>(div{${project.title}}))`
             ).join('+')})`, false)
         //
         // clients
-        view.expandAppend('section>(h2.section-title>small{worked}+{for})+ul.unstyled.clients', false)
+        view.expandAppend('section.for>(h2.section-title>small{worked}+{for})+ul.unstyled.svg-list.clients', false)
         const clients = ['sikkens', '72', 'uva', 'resn', 'tddb', 'novartis', 'randstad', '2x4', 'vodafone', 'bia', 'philips']
         Promise.all(clients.map(s=>fetch(`${MEDIA_URI}clients_${s}.svg`).then(rs=>rs.text())))
             .then(clients=>{
-              view.querySelector('.clients').insertAdjacentHTML('beforeend', clients.map(s=>`<li class="svgzoom">${s}</li>`).join(''))
+              view.querySelector('.clients').insertAdjacentHTML('beforeend', clients.map(s=>`<li>${s}</li>`).join(''))
+            })
+        //
+        // won
+        view.expandAppend('section.won>(h2.section-title>small{prizes}+{won})+ul.unstyled.svg-list.prizes', false)
+        const prizes = ['fwa', 'adcn', 'webby']
+        Promise.all(prizes.map(s=>fetch(`${MEDIA_URI}prizes_${s}.svg`).then(rs=>rs.text())))
+            .then(clients=>{
+              view.querySelector('.prizes').insertAdjacentHTML('beforeend', clients.map(s=>`<li>${s}</li>`).join(''))
             })
         //
         // blog
         const firstTen = posts.slice(0, 10)
         const getLi = post=>`(li>a[href="/${post.slug}"]>(time{${post.date.split('T').shift()}}+{${post.title}}))`
-        view.expandAppend(`section>(h2.section-title>small{articles}+{written})+ul.unstyled.link-list>(${firstTen.map(getLi).join('+')})`, false)
+        view.expandAppend(`section.written>(h2.section-title>small{articles}+{written})+ul.unstyled.link-list>(${firstTen.map(getLi).join('+')})`, false)
         return page
       })
   })
