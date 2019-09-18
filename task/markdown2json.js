49d6dc8c91af916d6a4433f7613482557be04cae
Ron Valstar <ron.valstar@randstadgroep.nl>
2019-09-18 13:17:49 +0200

    refactored meta to markdown in temp

--- task/markdown2json.js
+++ task/markdown2json.js
@@ -1,47 +1,58 @@
 const {promisify} = require('util')
 const glob = promisify(require('glob'))
 const utils = require('./util/utils.js')
-const {save,read} = utils
+const {save, read} = utils
 
 const postmeta = require('../temp/rv_postmeta')
-const metaMap = {_yoast_wpseo_title:'metaTitle',_yoast_wpseo_metadesc:'metaDescription',_yoast_wpseo_focuskw:'metaKeyword'}
-const validMetas = ['_yoast_wpseo_title','_yoast_wpseo_metadesc','_yoast_wpseo_focuskw']
-const data = postmeta[2].data.filter(({meta_key,meta_value})=>meta_value&&validMetas.includes(meta_key))
+const metaMap = {_yoast_wpseo_title:'metaTitle', _yoast_wpseo_metadesc:'metaDescription', _yoast_wpseo_focuskw:'metaKeyword'}
+const validMetas = ['_yoast_wpseo_title', '_yoast_wpseo_metadesc', '_yoast_wpseo_focuskw']
+const data = postmeta[2].data.filter(({meta_key, meta_value})=>meta_value&&validMetas.includes(meta_key))
 
-const markdownKeys = ['title','content']
-const arrayKeys = ['tags','categories','collaboration','clients','prizes','images']
+const markdownKeys = ['title', 'content']
+const arrayKeys = ['tags', 'categories', 'collaboration', 'clients', 'prizes', 'images']
 
 glob('src/data/markdown/+(post|fortpolio|page)_*.md')
     .then(files=>Promise.all(files.map(read)))
     .then(files=>files.map(markdown2object))
 
     .then(addMetaData)
-// return false;Promise.resolve()
+    // return false;Promise.resolve()
 
-    .then(saveObjectsToJSON)
+    // .then(saveObjectsToJSON)
     .then(saveObjectsToMarkdown)
 
 // read('src/data/markdown/post_tissue.md').then(markdown2object)
 // read('src/data/markdown/page_about.md').then(markdown2object)
 // read('src/data/markdown/fortpolio_dustin-kershaw.md').then(markdown2object)
 
+/**
+ * Convert markdown string data to an object literal with meta data
+ * @param {string} contents
+ * @returns {object}
+ */
 function markdown2object(contents){
   const lines = contents.trim().split(/\r\n|\r|\n/g)
   const hasComments = /^\s*<!--\s*$/.test(lines[0])
   const endComments = hasComments?firstMatchIndex(lines, /^\s*-->\s*$/):-1
   const metaLines = hasComments&&lines.slice(1, endComments)||[]
-  const meta = metaLines.reduce((acc,line)=>{
-    const [key,value] = line.trim().split(/\s*:\s*/)
-    acc[key] = arrayKeys.includes(key)?value.split(/,/).filter(o=>o):value
+  const meta = metaLines.reduce((acc, line)=>{
+    const [key, value] = line.trim().split(/\s*:\s*/)
+    acc[key] = arrayKeys.includes(key)?value.split(/,\s*/).filter(o=>o):value
     return acc
   }, {})
   const contentLines = lines.slice(endComments+1)
   const titleIndex = firstMatchIndex(contentLines, /^\s*#\s(.*)$/)
   const title = titleIndex!==-1&&contentLines[titleIndex].match(/#(.*)/).pop()||''
   const content = contentLines.slice(titleIndex+1).join('\n').trim()
-  return Object.assign(meta,{title,content})
+  return Object.assign(meta, {title, content})
 }
 
+/**
+ * Search an array for the first index to match
+ * @param {Array} a
+ * @param {RegExp} r
+ * @returns {number}
+ */
 function firstMatchIndex(a, r){
   let matchedIndex = -1
   for (let i=0, l=a.length;i<l;i++){
@@ -53,16 +64,26 @@ function firstMatchIndex(a, r){
   return matchedIndex
 }
 
-function saveObjectsToJSON(objects){
+/**
+ * Save the page-object to json
+ * @param {object[]} objects
+ * @returns {object[]}
+ */
+function saveObjectsToJSON(objects){ // eslint-disable-line no-unused-vars
   objects.forEach(obj=>{
     if (obj.slug&&obj.type){
       const fileName = `temp/json/${obj.type}_${obj.slug}.json`
-      save(fileName,JSON.stringify(obj))
+      save(fileName, JSON.stringify(obj))
     }
   })
   return objects
 }
 
+/**
+ * Add metadata to page-objects and adds meta-data
+ * @param {object[]} objects
+ * @returns {object[]}
+ */
 function addMetaData(objects){
   objects.forEach(obj=>{
     const {id} = obj
@@ -70,11 +91,15 @@ function addMetaData(objects){
     meta.forEach(metaObj=>{
       obj[metaMap[metaObj.meta_key]] = metaObj.meta_value
     })
-    // console.log('meta',meta); // todo: remove log
   })
   return objects
 }
 
+/**
+ * Save page-objects to markdown
+ * @param {object[]} objects
+ * @returns {object[]}
+ */
 function saveObjectsToMarkdown(objects){
   objects.forEach(obj=>{
     if (obj.slug&&obj.type){
@@ -82,7 +107,7 @@ function saveObjectsToMarkdown(objects){
       const markdown = `<!--
 ${Object.entries(obj).filter(([key])=>!markdownKeys.includes(key)).map(n=>{
   const [key, value] = n
-  if (arrayKeys.includes(key)) n[1] = value.join(',')
+  if (arrayKeys.includes(key)) n[1] = value.join(', ')
   return '  '+n.join(': ')
       }).join('\n')}
 -->
@@ -91,7 +116,7 @@ ${Object.entries(obj).filter(([key])=>!markdownKeys.includes(key)).map(n=>{
 
 ${content}`
       const fileName = `temp/markdown/${obj.type}_${obj.slug}.md`
-      save(fileName,markdown)
+      save(fileName, markdown)
     }
   })
   return objects
