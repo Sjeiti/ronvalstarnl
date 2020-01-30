2fb86b6eab14849cd36531aa3328ac1e9f3966a6
Ron Valstar <github@sjeiti.com>
2020-01-30 21:51:50 +0100

    header jsdoc and experiment randomisation

--- src/js/component/Header.js
+++ src/js/component/Header.js
@@ -18,6 +18,10 @@ create('[data-header]', class extends BaseComponent{
   _stuck = signal()
   _lastScrollTop = 0
   _lastHeaderTop = 0
+  _className = { 
+    stuck: 'stuck'
+    ,current: 'current'
+  }
 
   constructor(...args){
     super(...args)
@@ -31,6 +35,9 @@ create('[data-header]', class extends BaseComponent{
     this._background = this._element.querySelector('.background')
   }
 
+  /**
+   * Initialise experiments
+   */
   _initExperiments(){
     this._experimentWrapper = this._select('.experiment-wrapper')
     this._experimentUI = this._select('.experiment-ui')
@@ -40,8 +47,11 @@ create('[data-header]', class extends BaseComponent{
     this._stuck.add(is=>this._experiment?.pause(is))
   }
 
+  /**
+   * Set or remove the header image
+   * @param {string} src
+   */
   setImage(src){
-    console.log('setImage src', src) // todo: remove log
     if (src){
       this._background.style.backgroundImage = `url("${MEDIA_URI_HEADER+src}")`
     } else {
@@ -49,14 +59,20 @@ create('[data-header]', class extends BaseComponent{
     }
   }
 
+  /**
+   * Reveal header sticky state by adding/removing classNames
+   * @param {Event} e
+   * @param {number} w
+   * @param {number} h
+   */
   _onScroll(e, w, h){
-    const header = this._element//document.querySelector('header')
+    const header = this._element
     const headerTop = header.getBoundingClientRect().top
     if (this._lastScrollTop!==h){
       const isStuck = h>0&&headerTop===this._lastHeaderTop
-      const wasStuck = header.classList.contains('stuck')
+      const wasStuck = header.classList.contains(_className.stuck)
       if (wasStuck!==isStuck){
-        header.classList.toggle('stuck', isStuck)
+        header.classList.toggle(_className.stuck, isStuck)
         this._stuck.dispatch(isStuck)
       }
     }
@@ -66,15 +82,24 @@ create('[data-header]', class extends BaseComponent{
     header.style.backgroundPosition = `0 ${h/2}px`
   }
 
+  /**
+   * Handle routeChange signal
+   * @param {string} name
+   * @param {object} page
+   * @param {string} oldName
+   */
   _onRouteChange(name, page, oldName){
-    const current = 'current'
     const select = page.parentSlug||name
-    this._seldo('.'+current, elm=>elm.classList.remove(current))
-    this._seldo(`a[href="/${select}"]`, elm=>elm.classList.add(current))
+    this._seldo('.'+_className.current, elm=>elm.classList.remove(_className.current))
+    this._seldo(`a[href="/${select}"]`, elm=>elm.classList.add(_className.current))
     this._setExperiment(name, oldName)
     this.setImage()
   }
 
+  /**
+   * Show or hide experiment depending on page
+   * @param {string} name
+   */
   _setExperiment(name){
     const isExperiment = this._isExperiment(name)
     const experimentName = isExperiment&&name.replace(/^experiment-/, '')
@@ -89,19 +114,27 @@ create('[data-header]', class extends BaseComponent{
       this._experiment.exit()
       this._experiment = null
     } else if (!name&&!this._experiment){
-      this._experiment = Object.values(experiments).sort(()=>Math.random()<0.5?1:-1).pop()
+      this._experiment = experiments[Math.floor(Math.random()*experiments.length)]
+      //this._experiment = Object.values(experiments).sort(()=>Math.random()<0.5?1:-1).pop()
       this._experiment?.init(this._experimentWrapper)
     }
     this._experimentUI.classList.toggle('experiment-ui-hide', !this._experiment)
     this._experimentLink.href = this._experiment?`/experiment-${this._experiment.name}`:'#'
   }
 
+  /**
+   * Test if page is experiment by slug prefix
+   * @param {string} name
+   * @returns boolean
+   */
   _isExperiment(name){
     return /^experiment-.+/.test(name)
   }
 
+  /**
+   * Handle fullscreen click
+   */
   _onClickLink(){
-    console.log('_onClickLink') // todo: remove log
     document.body.matches('[data-pathname^="experiment-"]')
       &&this._experimentWrapper.requestFullscreen()
   }
