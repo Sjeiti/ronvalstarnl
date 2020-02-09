18cb31e075ed1fd0eb701bb216001fd32ead210f
Ron Valstar <ron@ronvalstar.nl>
2020-02-09 20:39:57 +0100

    don't show future blog posts, fix home heading underline

--- src/js/views/blog.js
+++ src/js/views/blog.js
@@ -6,8 +6,10 @@ add('blog', (view/*, route*/)=>
     fetch('/data/json/posts-list.json')
       .then(rs=>rs.json())
       .then(posts=>{
-        const firstTen = posts.slice(0, 10)
-        const theRest = posts.slice(9)
+        const today = new Date
+        const currentPast = posts.filter(({date})=>(new Date(date))<=today)
+        const firstTen = currentPast.slice(0, 10)
+        const theRest = currentPast.slice(9)
         const getLi = post=>`(li>a[href="/${post.slug}"]>(time{${post.date.split('T').shift()}}+{${post.title}}))`
         const ul = view.expandAppend(`ul.unstyled.link-list>(${firstTen.map(getLi).join('+')})`).querySelector('ul.link-list')
         requestAnimationFrame(()=>ul.insertAdjacentHTML('beforeend', expand(theRest.map(getLi).join('+'))))
