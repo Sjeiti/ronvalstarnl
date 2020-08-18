45621e9d0c5c6a2414c1daacb38154a4690d4c60
Ron Valstar <ron@ronvalstar.nl>
2020-08-18 11:34:47 +0200

    update cloudinary version nr

--- src/js/config.js
+++ src/js/config.js
@@ -1,7 +1,7 @@
 export const VERSION = _VERSION
 export const ENV = _ENV
 export const ENV_DEVELOPMENT = ENV['development']||false
-export const MEDIA_URI = 'https://res.cloudinary.com/dn1rmdjs5/image/upload/v1566568756/rv/'
+export const MEDIA_URI = 'https://res.cloudinary.com/dn1rmdjs5/image/upload/v1597739261/rv/'
 export const MEDIA_URI_HEADER = MEDIA_URI.replace('upload/', 'upload/c_scale,w_768/c_crop,w_768,h_200/')
 export const MEDIA_URI_THUMB = MEDIA_URI.replace('upload/', 'upload/c_scale,w_316/')
 export const MEDIA_URI_PROJECT = MEDIA_URI.replace('upload/', 'upload/c_scale,w_768/')
