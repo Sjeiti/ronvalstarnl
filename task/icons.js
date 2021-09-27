94472570df708f9e00500f6f0dfd447a887a9b3a
Ron Valstar <ron@ronvalstar.nl>
2021-09-27 10:37:07 +0200

    flippin icons

--- task/icons.js
+++ task/icons.js
@@ -1,5 +1,6 @@
 const {Command} = require('commander')
 const sharp = require('sharp')
+const mkdirp = require('mkdirp')
 const commander = new Command()
     .usage('[options] <files ...>')
     .option('--source [source]', 'Source path')
@@ -11,10 +12,11 @@ const sizeValues = sizes.split(/,/g).map(size=>size.split(/x/).map(parseFloat))
 
 sizeValues.forEach(o=>o.length===1&&o.push(o[0]))
 
-Promise.all(sizeValues.map(([width, height]) => {
-  const density = 72 * width / 16
-  return sharp(source, {density})
-      .resize(width, height)
-      .toFile(`${target}/icon-${width}x${height}.png`)
-}))
-
+mkdirp(target).then(() =>
+    Promise.all(sizeValues.map(([width, height]) => {
+      const density = 72 * width / 16
+      return sharp(source, {density})
+          .resize(width, height)
+          .toFile(`${target}/icon-${width}x${height}.png`)
+    }))
+)
