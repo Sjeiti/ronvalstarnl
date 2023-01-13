432e9ee420aa70501e7c6d890968bc3c91cda96c
Ron Valstar <ron@ronvalstar.nl>
2023-01-13 17:50:42 +0100

    added buildSVG.js

---
+++ task/buildSVG.js
@@ -0,0 +1,112 @@
+const {promisify} = require('util')
+const glob = promisify(require('glob'))
+const utils = require('./util/utils.js')
+const {markdown2object} = require('./util/markdown2object')
+const {read, save} = utils
+const { DOMImplementation, XMLSerializer } = require('xmldom')
+
+const subjects = ['PureMVC', 'JavaScript', 'jQuery', 'Angular', 'React', 'Vue', 'Backbone']
+const sbjescts = ['#2980B9', '#FF0044', '#0769AD', '#E23137', '#149ECA', '#41B783', '#0071B5']
+
+writeSVG()
+
+/**
+ * Write an SVG based on markdown data
+ * @returns {Promise<void>}
+ */
+async function writeSVG() {
+  const list = await glob('src/data/markdown/fortpolio_*.md')
+  const files = await Promise.all(list.map(read))
+  const objects = files.map(markdown2object)
+      .filter(({tags})=>{
+        return tags.filter(value => subjects.includes(value)).length>0
+      })
+  console.log('objects', objects[3]) // todo: remove log
+
+  const data = subjects.reduce((acc, name)=>{
+    acc[name] = {
+      name, ranges: []
+    }
+    return acc
+  }, {})
+
+  const allRanges = []
+
+  objects.forEach(({dateFrom, dateTo, tags})=>{
+    const from = getMillis(dateFrom)
+    const to = getMillis(dateTo)
+    allRanges.push(from, to)
+    tags.forEach(tag=>data[tag]?.ranges.push({from, to}))
+  })
+
+  const min = Math.min(...allRanges)
+  const max = Math.max(...allRanges)
+
+  const xmlSerializer = new XMLSerializer()
+  const markupSVG = xmlSerializer.serializeToString(drawSVG(data, min, max))
+  await save('temp/graph.svg', markupSVG)
+}
+
+/**
+ * Convert date string to millis number
+ * @param {string} date
+ * @returns {number}
+ */
+function getMillis(date){
+  return new Date(date).getTime()
+}
+
+/**
+ * Draw graph based on data
+ * @param {object} data
+ * @param {number} min
+ * @param {number} max
+ * @returns {Element}
+ */
+function drawSVG(data, min, max){
+  console.log('data',data) // todo: remove log
+  const document = new DOMImplementation().createDocument('http://www.w3.org/1999/xhtml', 'html', null)
+  const svgns = 'http://www.w3.org/2000/svg'
+  const svg = document.createElementNS(svgns, 'svg')
+  const width = 256
+  svg.setAttributeNS(null, 'width', width)
+  svg.setAttributeNS(null, 'height', 128)
+  // document.body.appendChild(svg)
+  //
+  const shape = document.createElementNS(svgns, 'circle')
+  shape.setAttributeNS(null, 'cx', 25)
+  shape.setAttributeNS(null, 'cy', 25)
+  shape.setAttributeNS(null, 'r',  20)
+  shape.setAttributeNS(null, 'fill', 'green')
+
+  //svg.appendChild(shape)
+
+  //path.setAttributeNS(null, 'd',  'M 0 128 C 64 128, 64 30, 128 30 C 128 30, 256 30, 256 30')
+  const margin = 16
+  const y = 128 - 2
+  function drawPath(svg, from, to, size, color) {
+    const path = document.createElementNS(svgns, 'path')
+    //path.setAttributeNS(null, 'd',  'M 0 128 C 64 128, 64 30, 128 30 L 256 30')
+    path.setAttributeNS(null, 'd',  `M ${from} ${y} C ${from+margin} ${y}, ${from+margin} ${y-size}, ${from+2*margin} ${y-size} L ${to-2*margin} ${y-size} C ${to-margin} ${y-size} ${to-margin} ${y} ${to} ${y} `)
+    path.setAttributeNS(null, 'fill', 'transparent')
+    path.setAttributeNS(null, 'stroke', color)
+    path.setAttributeNS(null, 'stroke-width', '2')
+    svg.appendChild(path)
+  }
+  drawPath(svg, 0,256,30, 'red')
+  drawPath(svg, 10,128,50, 'green')
+  drawPath(svg, 10,256,47, 'blue')
+
+  ////////////////////
+  Object.values(data).forEach(({name, ranges})=>{
+    ranges.forEach(({from, to})=>{
+      const size = max - min
+      const x1 = (from - min)/size*width
+      const x2 = (to - min)/size*width
+      drawPath(svg, x1,x2,30, 'lime')
+    })
+  })
+  ////////////////////
+
+  return svg
+}
--- task/markdown2json.js
+++ task/markdown2json.js
@@ -2,15 +2,14 @@ const {marked} = require('marked')
 const {promisify} = require('util')
 const glob = promisify(require('glob'))
 const utils = require('./util/utils.js')
+const {markdown2object} = require('./util/markdown2object')
 const {save, read} = utils
 
-const arrayKeys = ['tags', 'categories', 'collaboration', 'clients', 'prizes', 'images']
-const booleanKeys = ['inCv', 'inPortfolio', 'sticky']
 const t = s=>(new Date(s)).getTime()||Number.MAX_VALUE
 
 marked.use({ renderer: { html: s => s } })
 
-;(async ()=>{
+;(async()=>{
   const files = await glob('src/data/markdown/+(post|fortpolio|page)_*.md')
   const contents = await Promise.all(files.map(read))
   const objects = contents.map(markdown2object)
@@ -18,47 +17,6 @@ marked.use({ renderer: { html: s => s } })
   saveObjectsToJSON(objects)
   saveObjectsToLists(objects)
 })()
-
-/**
- * Convert markdown string data to an object literal with meta data
- * @param {string} contents
- * @returns {object}
- */
-function markdown2object(contents){
-  const lines = contents.trim().split(/\r\n|\r|\n/g)
-  const hasComments = /^\s*<!--\s*$/.test(lines[0])
-  const endComments = hasComments?firstMatchIndex(lines, /^\s*-->\s*$/):-1
-  const metaLines = hasComments&&lines.slice(1, endComments)||[]
-  const meta = metaLines.reduce((acc, line)=>{
-    const [key, value] = line.trim().split(/\s*:\s*(.*)/)
-    if (arrayKeys.includes(key)) acc[key] = value.split(/,\s*/).filter(o=>o)
-    else if (booleanKeys.includes(key)) acc[key] = value==='true'
-    else acc[key] = value
-    return acc
-  }, {})
-  const contentLines = lines.slice(endComments+1)
-  const titleIndex = firstMatchIndex(contentLines, /^\s*#\s(.*)$/)
-  const title = (titleIndex!==-1&&contentLines[titleIndex].match(/#(.*)/).pop()||'').trim()
-  const content = marked(contentLines.slice(titleIndex+1).join('\n').trim(), {breaks: true/*, gfm: true*/})
-  return Object.assign(meta, {title, content})
-}
-
-/**
- * Search an array for the first index to match
- * @param {Array} a
- * @param {RegExp} r
- * @returns {number}
- */
-function firstMatchIndex(a, r){
-  let matchedIndex = -1
-  for (let i=0, l=a.length;i<l;i++){
-    if (r.test(a[i])){
-      matchedIndex = i
-      break
-    }
-  }
-  return matchedIndex
-}
 
 /**
  * Save the page-object to json
