c550d30ea37255be5e6d834491ef9c1b0dde693b
Ron Valstar <ron@ronvalstar.nl>
2023-08-07 09:19:15 +0200

    Set star icon aria label

--- src/js/component/Skill.js
+++ src/js/component/Skill.js
@@ -23,16 +23,25 @@ export function applySkillSVG(parent){
   Array.from(new Array(5)).forEach(()=>{
     const svg = document.createElementNS(NS_SVG, 'svg')
     svg.setAttribute('data-icon', 'star')
+    svg.setAttribute('aria-label', 'star')
+    svg.setAttribute('inert', '')
     parent.insertBefore(svg, parent.firstChild)
   })
   //
   const skill = parseFloat(parent.dataset.skill)
   const skillPercentage = skill*20
   const wrap = createElement('div', null, parent)
+
   wrap.style.clipPath = `polygon(0 0, ${skillPercentage}% 0, ${skillPercentage}% 100%, 0% 100%)`
   Array.from(new Array(5)).forEach(()=>{
     const svg = document.createElementNS(NS_SVG, 'svg')
     svg.setAttribute('data-icon', 'star')
+    svg.setAttribute('aria-label', 'star')
+    svg.setAttribute('inert', '')
+    // svg.setAttribute('role', 'none')
+    // svg.setAttribute('aria-hidden', 'true')
     wrap.appendChild(svg)
   })
+
+  createElement('span', 'visually-hidden', wrap, null, skill+' stars')
 }
