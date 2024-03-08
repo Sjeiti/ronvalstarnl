0be1dc4eaa1783a04fc2993509aaf06a0acf25a5
Ron Valstar <ron@ronvalstar.nl>
2024-03-08 21:34:46 +0100

    Striked freelance in first home paragraph

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -17,64 +17,52 @@ add(
   , 'home'
   , (view, route)=>{
     return fetchJSONFiles(`page_${route}`, 'posts-list', 'fortpolio-list')
-      .then(([page, posts, projects])=>{
-        view.appendString(page.content)
-        //
-        // projects // todo duplicate code in `view/projects.js`
-        const projectHighlight = ['strange-attractors-javascript', 'kees-kroot', 'disconnect', 'project-invoice'].map(slug=>projects.filter(p=>p.slug===slug).pop())
-        view.expandAppend(`section.built>(h2.section-title>small{projects}+{built})+ul.unstyled.projects>(${projectHighlight.map(getProjectThumbZen).join('+')})`, false)
-        //
-        // clients
-        view.expandAppend('section.for>(h2.section-title>small{worked}+{for})+ul.unstyled.svg-list.clients', false)
-        const clientNames = [
-              'sikkens'
-            , 'uva'
-            , 'tudelft'
-            , 'wageningen'
-            , 'randstad'
-            , 'philips'
-            , 'vodafone'
-            , '2x4'
-            , 'novartis'
-            , 'resn'
-            , 'buildinamsterdam'
-            , '72andsunny'
-            , 'tribalddb'
-          ]
-        const elmClients = view.querySelector('.clients')
-        populateSVGList(elmClients, 'client-', clientNames)
-        setTimeout(()=>{
-            const {children: [{offsetWidth}], style} = elmClients
-            elmClients.style.width = `${clientNames.length*offsetWidth}px`
-            //
-            style.marginLeft = 0
-            TweenMax.to(
-                style
-                , 6
-                , {
-                  marginLeft: `${-offsetWidth}px`
-                  , onRepeat: () => elmClients.appendChild(elmClients.children[0])
-                  , repeat: -1
-                  , ease: Linear.easeNone
-                }
-            )
-        }, 140)
-        //
-        // won
-        view.expandAppend('section.won>(h2.section-title>small{prizes}+{won})+ul.unstyled.svg-list.prizes', false)
-        const prizes = ['fwa', 'adcn', 'webby']
-        const elmPrizes = view.querySelector('.prizes')
-        populateSVGList(elmPrizes, 'prize-', prizes)
-        //
-        // blog
-        const currentPast = stickiesFirst(posts.filter(todayOlderFilter))
-        const firstTen = currentPast.slice(0, 10)
-        const [first, ...rest] = firstTen.filter(p=>!p.sticky)
-        const sticked = [first, ...firstTen.filter(p=>p.sticky)]
-        const getLi = post=>`(li>a[href="/${post.slug}"]>(time{${post.date.split('T').shift()}}+{${post.title}}))`
-        const stickies = `(.paper>div>ul.unstyled.link-list>(${sticked.map(getLi).join('+')}))`
-        const others = `ul.unstyled.link-list>(${rest.map(getLi).join('+')})`
-        view.expandAppend(`section.written>(h2.section-title>small{articles}+{written})+${stickies}+${others}`, false)
-        return page
+      .then(([page, posts, projects])=> {
+          view.appendString(page.content)
+          //
+          // projects // todo duplicate code in `view/projects.js`
+          const projectHighlight = ['strange-attractors-javascript','kees-kroot','disconnect','project-invoice'].map(slug => projects.filter(p => p.slug===slug).pop())
+          view.expandAppend(`section.built>(h2.section-title>small{projects}+{built})+ul.unstyled.projects>(${projectHighlight.map(getProjectThumbZen).join('+')})`,false)
+          //
+          // working
+          // view.expandAppend(`section.for>(h2.section-title>small{working}+{at})+ul.unstyled.svg-list.at>li>svg[aria-label=volksbank]>use[xlink:href="#client-philips"]`,false)
+          //
+          //<li><svg aria-label="philips"><use xlink:href="#client-philips"></use></svg></li>
+          //li>svg[aria-label=volksbank]>use[xlink:href="#client-philips"]
+          //
+          // clients
+          view.expandAppend('section.for>(h2.section-title>small{worked}+{for})+ul.unstyled.svg-list.clients',false)
+          const clientNames = ['sikkens','uva','tudelft','wageningen','randstad','philips','vodafone','2x4','novartis','resn','buildinamsterdam','72andsunny','tribalddb']
+          const elmClients = view.querySelector('.clients')
+          populateSVGList(elmClients,'client-',clientNames)
+          setTimeout(() => {
+              const {children: [{offsetWidth}],style} = elmClients
+              elmClients.style.width = `${clientNames.length * offsetWidth}px`
+              //
+              style.marginLeft = 0
+              TweenMax.to(style,6,{
+                  marginLeft: `${-offsetWidth}px`,
+                  onRepeat: () => elmClients.appendChild(elmClients.children[0]),
+                  repeat: -1,
+                  ease: Linear.easeNone
+              })
+          },140)
+          //
+          // won
+          view.expandAppend('section.won>(h2.section-title>small{prizes}+{won})+ul.unstyled.svg-list.prizes',false)
+          const prizes = ['fwa','adcn','webby']
+          const elmPrizes = view.querySelector('.prizes')
+          populateSVGList(elmPrizes,'prize-',prizes)
+          //
+          // blog
+          const currentPast = stickiesFirst(posts.filter(todayOlderFilter))
+          const firstTen = currentPast.slice(0,10)
+          const [first,...rest] = firstTen.filter(p => !p.sticky)
+          const sticked = [first,...firstTen.filter(p => p.sticky)]
+          const getLi = post => `(li>a[href="/${post.slug}"]>(time{${post.date.split('T').shift()}}+{${post.title}}))`
+          const stickies = `(.paper>div>ul.unstyled.link-list>(${sticked.map(getLi).join('+')}))`
+          const others = `ul.unstyled.link-list>(${rest.map(getLi).join('+')})`
+          view.expandAppend(`section.written>(h2.section-title>small{articles}+{written})+${stickies}+${others}`,false)
+          return page
       })
-  })
+    })
